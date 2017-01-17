b828d89633cba8600a7d6c2c8316afeac45a8762
Eduardo <e.chiaro@gmail.com>
2017-01-17 11:29:37 -0800

    Initial Commit

---
+++ myHood-dev/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myHood-dev
+//
+//  Created by Eduardo Chiaro on 1/17/17.
+//  Copyright © 2017 Eduardo Chiaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
